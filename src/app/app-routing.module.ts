34aebc4f775bbe0ddd12c6f1954ff27ea4954d5c
Rohit Kalva <rohit@kalva.info>
2021-01-29 10:18:10 +0100

    Move routes to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ProfileComponent
+  }
+];
+
+const config: ExtraOptions = {
+  useHash: true,
+};
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, config)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RouterModule, Routes, ExtraOptions } from '@angular/router';
 import { ProfileModule } from './profile/profile.module';
 import { HttpClientModule } from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { ProfileComponent } from './profile/profile.component';
 
-
-const routes: Routes = [
-  {
-    path: '',
-    component: ProfileComponent
-  }
-];
-
-const config: ExtraOptions = {
-  useHash: true,
-};
 
 @NgModule({
   declarations: [
@@ -28,8 +15,8 @@ const config: ExtraOptions = {
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(routes, config),
     ProfileModule,
     HttpClientModule
 
