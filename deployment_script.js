a29ee586012fd239db4a3432abf8cb400f37ab86
Rohit Kalva <rohit@kalva.info>
2021-01-29 10:33:54 +0100

    Update deployment_script.js

--- deployment_script.js
+++ deployment_script.js
@@ -2,4 +2,4 @@ const ghpages = require('gh-pages');
 const fs = require('fs');
 
 fs.writeFile('dist/Personal-Portfolio/CNAME', "kalva.info", function(err) {});
-ghpages.publish('dist', function(err) {});
+ghpages.publish('dist/Personal-Portfolio', function(err) {});
